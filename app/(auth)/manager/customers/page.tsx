e3f917491652cfd2b6b3d3767fc4379ed77c3fdf
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2024-05-01 04:28:55 +0530

    View all customers page

--- app/(auth)/manager/customers/page.tsx
+++ app/(auth)/manager/customers/page.tsx
@@ -1,23 +1,50 @@
 'use client';
 
 import { useQuery } from '@tanstack/react-query';
+import type * as Db from '@/types/dbSchema';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
 export default function ManagerCustomersPage() {
     const customersQuery = useQuery({
         queryKey: ['manager', 'claims'],
         queryFn: async () => {
             const res = await fetch('/api/manager/customers');
-            if(!res.ok) throw res.statusText;
+            if (!res.ok) throw res.statusText;
             const json = await res.json();
-            if('error' in json) throw json.error;
+            if ('error' in json) throw json.error;
 
-            return json.data;
+            return json.data as Db.Customer[];
         },
         refetchInterval: 5 * 60 * 1000
     });
-    if(customersQuery.isSuccess) console.log('customersQuery data:', customersQuery.data);
+    if (!customersQuery.isSuccess || !customersQuery.data) return <div></div>;
 
     return (
-        <></>
+        <div className='p-4'>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Id</TableHead>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Email</TableHead>
+                        <TableHead>Phone No.</TableHead>
+                        <TableHead>Gender</TableHead>
+                        <TableHead>PAN</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {customersQuery.data.map(customer => (
+                        <TableRow key={customer.customer_id}>
+                            <TableCell className="font-medium">{customer.customer_id}</TableCell>
+                            <TableCell>{customer.first_name} {customer.last_name}</TableCell>
+                            <TableCell>{customer.email}</TableCell>
+                            <TableCell>{customer.phone_no}</TableCell>
+                            <TableCell>{customer.gender}</TableCell>
+                            <TableCell>{customer.pan_no}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
     );
 }
