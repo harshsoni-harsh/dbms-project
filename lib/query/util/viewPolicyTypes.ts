17b9721bad37ed3d9aed7331749ec022b3302d95
Harsh Soni <52278293+harshsoni-harsh@users.noreply.github.com>
2024-05-01 17:53:52 +0530

    Updated queries in util

--- lib/query/util/viewPolicyTypes.ts
+++ lib/query/util/viewPolicyTypes.ts
@@ -1,12 +1,12 @@
 import dbConn from "@/lib/dbConnector";
 
 export async function viewPolicyTypes() {
-    const conn = await dbConn;
-    await conn.connect();
+  const conn = await dbConn;
+  await conn.connect();
 
-    const queryStatement = `
+  const queryStatement = `
     select * from policy_type;
   `;
-    const res = await conn.query(queryStatement);
-    return res;
+  const res = await conn.query(queryStatement);
+  return res;
 }
