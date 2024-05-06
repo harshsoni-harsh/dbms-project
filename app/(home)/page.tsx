0a3972c2b6dc4ec82737bf471dc5e019ea16f3ba
Harsh Soni <52278293+harshsoni-harsh@users.noreply.github.com>
2024-05-07 02:45:13 +0530

    Improved homepage UI

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,30 +1,30 @@
 import Image from "next/image";
-import car from "@/public/images/car-broken.jpg";
+import car from "@/public/images/car.png";
 import Link from "next/link";
 
 export default function Home() {
   const about = "Welcome to Motor Insurance, where we specialize in providing peace of mind through our comprehensive insurance coverage. With years of experience and a commitment to customer satisfaction, we're here to safeguard what matters most to you";
   const heading = "Insure Your Journey: Navigate Life's Roads Safely with Our Vehicle Insurance!"
   return (
-    <div className="flex items-center justify-start text-justify w-full h-full overflow-hidden">
-      <div className="flex flex-col items-center justify-between w-full md:w-1/2 gap-2 h-1/2 rounded-md p-2">
+    <div className="flex max-md:flex-col items-center justify-center text-justify w-full h-full p-8">
+      <div className="flex flex-col items-center justify-center w-full md:w-1/2 gap-8 rounded-md max-md:order-2">
         <span
-          className="text-3xl font-extrabold justify-self-start mb-4"
+          className="text-3xl font-extrabold justify-self-start mb-4 opacity-80 text-left"
         >
           {heading}
         </span>
-        <p className="text-xl ">
+        <p className="text-xl text-left">
           {about}
         </p>
         <Link
           href={'./about'}
-          className="w-fit p-2 min-h-10 flex items-center justify-center font-medium text-center rounded-md text-background bg-foreground"
+          className="w-fit p-2 min-h-10 flex items-center justify-center font-medium text-center rounded-md text-background bg-foreground opacity-80"
         >
           About Us
         </Link>
       </div>
-      <div className="absolute -z-10">
-        <Image src={car} alt={"Car Image"} />
+      <div className="invert opacity-80 max-md:order-1 my-6 flex-shrink max-w-80">
+        <Image src={car} alt="Car Image" />
       </div>
     </div >
   );
